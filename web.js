94b84ec57a4976f67356b5a564124406a0849d01
kostyan <kozzztya@gmail.com>
2016-06-24 17:46:21 +0300

    Disable cors

--- web.js
+++ web.js
@@ -1,7 +1,9 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var cors = require('cors');
 
+app.use(cors())
 app.get('*', function(request, response) {
   response.redirect(process.env.NEW_BASE_URL + request.url)
 });
