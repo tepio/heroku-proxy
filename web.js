2942a25765a62ef693f969ba606de56a8b85d831
kostyan <kozzztya@gmail.com>
2016-07-14 18:14:08 +0300

    Add proxy

--- web.js
+++ web.js
@@ -1,14 +1,19 @@
 var express = require('express');
+var proxy = require('http-proxy-middleware');
 
 var app = express.createServer(express.logger());
 var cors = require('cors');
 
-app.use(cors())
-app.get('*', function(request, response) {
-  response.redirect(process.env.NEW_BASE_URL + request.url)
-});
+app.use(cors());
+
+if (process.env.NEW_BASE_URL) {
+  app.get('*', function(request, response) {
+    response.redirect(process.env.NEW_BASE_URL + request.url)
+  });
+  app.use('/', proxy({ target: process.env.NEW_BASE_URL, changeOrigin: true }));
+}
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
+app.listen(port, function () {
   console.log("Listening on " + port);
 });
